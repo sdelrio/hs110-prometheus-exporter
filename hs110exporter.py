e033ef47cdb16fb0bec14c14f36a2c10dcc6086b
Sergio R <oftheriver@gmail.com>
2019-06-13 23:41:04 +0200

    Refactor. Create HS110data Class

--- hs110exporter.py
+++ hs110exporter.py
@@ -12,32 +12,10 @@ import socket
 import argparse
 import json
 
-version = 0.81
+version = 0.9
 
-keyname = {
-    "h1": { # Hardware version 1.x
-        "current": "current",
-        "voltage": "voltage",
-        "power": "power",
-        "total": "total"
-    },
-    "h2": { # Hardware version 2.x
-        "current": "current_ma",
-        "voltage": "voltage_mv",
-        "power": "power_mw",
-        "total": "total_wh"
-    }
-}
-
-# Default HS110 hardware version
-hardware = "h2"
-
-# Encryption and Decryption of TP-Link Smart Home Protocol
-# XOR Autokey Cipher with starting key = 171
-hs110_key = 171
-
-# Check if IP is valid
 def validIP(ip):
+    """ Check type format and valid IP for input parameter """
     if type(ip) not in [str]:
         raise TypeError("The IP parameter must be a string")
 
@@ -49,44 +27,112 @@ def validIP(ip):
         raise ValueError("Invalid IP Address.")
     return ip
 
-# Encryption and Decryption of TP-Link Smart Home Protocol
-# XOR Autokey Cipher with starting key = 171
+class HS110data:
+    """ Storage and management for HS110 data """
+    def __init__(self, hardware_version='h2'):
+        """ Constructor for HS110 data
+        hardware_version: defaults to 'h2' can also be 'h1' """
 
-def encrypt(string):
-    if type(string) not in [str]:
-        raise TypeError("The encrypt parameter must be a string")
-    key = hs110_key
-    result =  b"\0\0\0" + bytes([len(string)])
-    for i in bytes(string.encode('latin-1')):
-        a = key ^ i
-        key = a
-        result += bytes([a])
-    return result
+        allowed_hardware = ['h1', 'h2']
+        if hardware_version in allowed_hardware:
+            self.__hardware = hardware_version
+        else:
+            raise ValueError("hardware_version must be " + ' or '.join(allowed_hardware))
 
-def decrypt(string):
-    if type(string) not in [bytes]:
-        raise TypeError("The decrypt parameter must be bytes")
-    string= string[4:]
-    key = hs110_key
-    result = b""
-    for i in bytes(string):
-        a = key ^ i
-        key = i
-        result += bytes([a])
-    return result.decode('latin-1')
+        self.__keyname = {
+            "h1": { # Hardware version 1.x
+                "current": "current",
+                "voltage": "voltage",
+                "power": "power",
+                "total": "total"
+            },
+            "h2": { # Hardware version 2.x
+                "current": "current_ma",
+                "voltage": "voltage_mv",
+                "power": "power_mw",
+                "total": "total_wh"
+            }
+        }
 
-def get_data(item):
-    """ Get item from HS110 array of values """
-    allowed_items = ['power', 'current', 'voltage', 'total']
-    if type(item) is not str:
-        raise TypeError('get_data parameter must be str type')
-    if item not in allowed_items:
-        raise ValueError('get_data parameter must be one of: [' + ', '.join(allowed_items) + ']')
-    try:
-        return received_data["emeter"]["get_realtime"][keyname[hardware][item]]
-    except socket.error:
-        quit("Could not connect to host " + ip + ":" + str(port))
-        return 0
+        self.received_data = self.__empty_data()
+
+        # Encryption and Decryption of TP-Link Smart Home Protocol
+        # XOR Autokey Cipher with starting key = 171
+        self.__hs110_key = 171
+
+    def __encrypt(self, string):
+        if type(string) not in [str]:
+            raise TypeError("The encrypt parameter must be a string")
+        key = self.__hs110_key
+        result =  b"\0\0\0" + bytes([len(string)])
+        for i in bytes(string.encode('latin-1')):
+            a = key ^ i
+            key = a
+            result += bytes([a])
+        return result
+
+    def __decrypt(self, string):
+        if type(string) not in [bytes]:
+            raise TypeError("The decrypt parameter must be bytes")
+        string= string[4:]
+        key = self.__hs110_key
+        result = b""
+        for i in bytes(string):
+            a = key ^ i
+            key = i
+            result += bytes([a])
+        return result.decode('latin-1')
+
+    def __str__(self):
+        return ', '.join(['{key}={value}'.format(key=key, value=self.received_data['emeter']['get_realtime'].get(key)) for key in self.received_data['emeter']['get_realtime']])
+
+    def __empty_data(self):
+        return {
+            "emeter": {
+                "get_realtime": {
+                self.__keyname[self.__hardware]['current']: 0,
+                self.__keyname[self.__hardware]['voltage']: 0,
+                self.__keyname[self.__hardware]['power']: 0,
+                self.__keyname[self.__hardware]['total']: 0,
+                "err_code":0
+                }
+            }
+        }
+
+    def receive(self, data):
+        """ Receive encrypted data, decrypts and stores into self.reived_data """
+        if type(data) is not bytes:
+            raise ValueError("receive_data parameter must be of type bytes")
+        try:
+            self.received_data = json.loads(self.__decrypt(data))
+            if "current_ma" in self.received_data['emeter']['get_realtime']:
+                self.__hardware = 'h2'
+            else:
+                self.__hardware = 'h1'
+        except:
+            raise ValueError("json.loads decrypt data")
+
+    def get_cmd(self):
+        """ Get encrypted command to get realtime info from HS110 """
+        cmd = '{"emeter":{"get_realtime":{}}}'
+        return self.__encrypt(cmd)
+
+    def get_data(self, item):
+        """ Get item (power, current, voltage or total) from HS110 array of values """
+        allowed_items = ['power', 'current', 'voltage', 'total']
+        if type(item) is not str:
+            raise TypeError('get_data parameter must be str type')
+        if item not in allowed_items:
+            raise ValueError('get_data parameter must be one of: [' + ', '.join(allowed_items) + ']')
+        try:
+            return self.received_data["emeter"]["get_realtime"][self.__keyname[self.__hardware][item]]
+        except socket.error:
+            quit("Could not connect to host " + ip + ":" + str(port))
+            return 0
+
+    def reset_data(self):
+        """ Reset self.received_data values to 0 """
+        self.received_data = self.__empty_data()
 
 # Main entry point
 if __name__ == '__main__':
@@ -114,6 +160,8 @@ if __name__ == '__main__':
         }
       }
     }
+    # Init object
+    hs110 = HS110data()
 
     # Send command and receive reply
 
@@ -126,11 +174,13 @@ if __name__ == '__main__':
     REQUEST_TOTAL   = Gauge('hs110_total', 'HS110 Energy measure')
 
 
-    REQUEST_POWER.set_function(lambda: get_data('power'))
-    REQUEST_CURRENT.set_function(lambda: get_data('current'))
-    REQUEST_VOLTAGE.set_function(lambda: get_data('voltage'))
-    REQUEST_TOTAL.set_function(lambda: get_data('total'))
+    REQUEST_POWER.set_function(lambda: hs110.get_data('power'))
+    REQUEST_CURRENT.set_function(lambda: hs110.get_data('current'))
+    REQUEST_VOLTAGE.set_function(lambda: hs110.get_data('voltage'))
+    REQUEST_TOTAL.set_function(lambda: hs110.get_data('total'))
 
+    print("Configuration IP: " + ip + ":" + str(port) )
+    print("Initialising with empty data: ",  hs110)
     # Start up the server to expose the metrics.
     start_http_server(listen_port)
 
@@ -141,24 +191,21 @@ if __name__ == '__main__':
 
         try:
             sock_tcp.connect((ip, port))
-            sock_tcp.send(encrypt(cmd))
+            sock_tcp.send(hs110.get_cmd())
             data = sock_tcp.recv(2048)
             sock_tcp.close()
+
             # Sample return value received:
             # HS110 Hardware 1: {"emeter":{"get_realtime":{"voltage":229865,"current":1110,"power":231866,"total":228,"err_code":0}}}
             # HS110 Hardware 2: {"emeter":{"get_realtime":{"voltage_mv":229865,"current_ma":1110,"power_mw":231866,"total_wh":228,"err_code":0}}}
-            received_data = json.loads(decrypt(data))
-            print(received_data)
-            if "current_ma" in received_data['emeter']['get_realtime']:
-                hardware = "h2"
-            else:
-                hardware = "h1"
-            print("IP: " + ip + ":" + str(port) + " Received power: " + str(received_data["emeter"]["get_realtime"][keyname[hardware]['power']]))
+
+            hs110.receive(data)  # Receive and decrypts data
+            print(hs110)
         except socket.error:
-            print("Could not connect to the host "+ ip + ":" + str(port))
+            print("Could not connect to the host "+ ip + ":" + str(port) + " Keeping last values")
         except ValueError:
-            received_data = {"emeter":{"get_realtime":{keyname[hardware]['voltage']:0,keyname[hardware]['current']:0,keyname[hardware]['power']:0,keyname[hardware]['total']:0,"err_code":0}}}
-            print("Could not decrypt data from hs110.")
+            hs110.reset_data()
+            print("Could not decrypt data from hs110. Reseting values.")
 
         time.sleep(sleep_time)
 
--- test_hs110exporter.py
+++ test_hs110exporter.py
@@ -2,7 +2,7 @@
 import unittest
 import socket
 
-from hs110exporter import validIP, encrypt, decrypt, get_data
+from hs110exporter import validIP, HS110data
 
 class TestValidIP(unittest.TestCase):
   def test_ipstring(self):
@@ -15,32 +15,50 @@ class TestValidIP(unittest.TestCase):
     self.assertRaises(TypeError, validIP, 3j)
     self.assertRaises(ValueError, validIP, '192.168.0.1.a')
 
-class TestEncrypt(unittest.TestCase):
+class TestHS110data(unittest.TestCase):
   def test_encryptstring(self):
+    hs110 = HS110data()
     text_encrypted = b'\x00\x00\x00\x10\xd0\xf0\x98\xfd\x91\xfd\x92\xa8\x88\xff\x90\xe2\x8e\xea\xca\xb7'
     text_decrypted = '{ hello: world }'
-    self.assertEqual(encrypt(text_decrypted), text_encrypted)
-    self.assertEqual(decrypt(text_encrypted), text_decrypted)
+
+    self.assertEqual(hs110._HS110data__encrypt(text_decrypted), text_encrypted)
+    self.assertEqual(hs110._HS110data__decrypt(text_encrypted), text_decrypted)
+
 
   def test_encryptvalues(self):
-    self.assertRaises(TypeError, encrypt, 100)
-    self.assertRaises(TypeError, encrypt, 100.1)
-    self.assertRaises(TypeError, encrypt, 3j)
-    self.assertRaises(TypeError, encrypt, b'\x00\x00\x00\x10')
-    self.assertIsInstance(encrypt('Hello world'), bytes)
+    hs110 = HS110data()
+
+    self.assertRaises(TypeError, hs110._HS110data__encrypt, 100)
+    self.assertRaises(TypeError, hs110._HS110data__encrypt, 100.1)
+    self.assertRaises(TypeError, hs110._HS110data__encrypt, 3j)
+    self.assertRaises(TypeError, hs110._HS110data__encrypt, b'\x00\x00\x00\x10')
+    self.assertIsInstance(hs110._HS110data__encrypt('Hello world'), bytes)
 
   def test_decryptvalues(self):
-    self.assertRaises(TypeError, decrypt, 100)
-    self.assertRaises(TypeError, decrypt, 100.1)
-    self.assertRaises(TypeError, decrypt, 3j)
-    self.assertRaises(TypeError, decrypt, "Hello world")
-    self.assertIsInstance(decrypt(b'\x00\x00\x00\x10'), str)
+    hs110 = HS110data()
+
+    self.assertRaises(TypeError, hs110._HS110data__decrypt, 100)
+    self.assertRaises(TypeError, hs110._HS110data__decrypt, 100.1)
+    self.assertRaises(TypeError, hs110._HS110data__decrypt, 3j)
+    self.assertRaises(TypeError, hs110._HS110data__decrypt, "Hello world")
+    self.assertIsInstance(hs110._HS110data__decrypt(b'\x00\x00\x00\x10'), str)
 
   def test_received_data(self):
-    self.assertRaises(TypeError, get_data, 100)
-    self.assertRaises(TypeError, get_data, 100.1)
-    self.assertRaises(TypeError, get_data, 3j)
-    self.assertRaises(ValueError, get_data, "nonexist")
+    hs110 = HS110data()
+
+    self.assertRaises(TypeError, hs110.get_data, 100)
+    self.assertRaises(TypeError, hs110.get_data, 100.1)
+    self.assertRaises(TypeError, hs110.get_data, 3j)
+    self.assertRaises(ValueError, hs110.get_data, "nonexist")
+
+  def test_constructor(self):
+    self.assertRaises(ValueError, HS110data, hardware_version='h999')
+    hs110 = HS110data('h1')
+    self.assertEqual(hs110._HS110data__hardware, 'h1')
+    hs110 = HS110data('h2')
+    self.assertEqual(hs110._HS110data__hardware, 'h2')
+    hs110 = HS110data()
+    self.assertEqual(hs110._HS110data__hardware, 'h2')
 
 if __name__ == '__main__':
     unittest.main()

58fb2212d45159fab0c8819d172bdf16fa671670
sdelrio <sdelrio@users.noreply.github.com>
2019-07-18 19:44:27 +0200

    Add Parameter types, hypothesis and contracts (#6)
    * tox force 90% code coverage
    
    * Add contracts and hypothesis to validIP
    
    * Remove python 2 compatibility
    
    * Add conntracts and hypothesis contrustor and port
    
    * Fix tcp port ranges
    
    * Add conntracts and hypothesis test_get_connection_info
    
    * Add conntracts and hypothesis test_encryptvalues
    
    * Add more contracts and hypothesis test_ecryptvalues
    
    * Add more contracts and hypothesis test_decryptvalues
    
    * Clean old checkings covered by  contracts and hypothesis
    
    * Types on constructor
    
    * Back to 100% converage on test_hs110exporter.py
    
    * More contracts and hypothesis get_data, receive
    
    * Clean some type tests. Limit patch on prints at tets.
    
    * Add mypy test. Add more types to code
    
    * Add coveragerc. Clean up some comment

--- hs110exporter.py
+++ hs110exporter.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Python 3 to python 2 compatibilty
-from __future__ import print_function
-from builtins import bytes
-
+from dpcontracts import require, ensure
 from prometheus_client import start_http_server, Gauge
 import struct
 import time
@@ -12,32 +9,36 @@ import socket
 import argparse
 import json
 
-version = 0.93
+from typing import Union
 
-def validIP(ip):
+version = 0.94
+
+@require("ip must be a string", lambda args: isinstance(args.ip, str))
+@require("ip must not be empty", lambda args: len(args.ip) > 0)
+@ensure("result is part of input", lambda args, result: result in args.ip )
+def validIP(ip: str) -> str:
     """ Check type format and valid IP for input parameter """
-    if type(ip) != str:
-        raise TypeError("The IP parameter must be a string")
 
     ip = ip.strip()  # Remove trailing spaces
 
     try:
         socket.inet_pton(socket.AF_INET, ip)
     except socket.error:
-        raise ValueError("Invalid IP Address.")
+        raise ValueError("Invalid IP Address %s" % ip)
     return ip
 
 class HS110data:
     """ Storage and management for HS110 data """
-    def __init__(self, hardware_version='h2', ip='192.168.1.53', port=9999):
+    @require("ip must be a valid IP", lambda args: validIP(args.ip))
+    @require("hardware_version must be string", lambda args: isinstance(args.hardware_version, str))
+    @require("hardware_version must be 'h1' or 'h2' ", lambda args: args.hardware_version in ['h1', 'h2'])
+    @require("port must be intenger", lambda args: isinstance(args.port, int) and args.port >= 0 and args.port <= 65535)
+    def __init__(self, hardware_version: str='h2', ip: str='192.168.1.53', port: int=9999) -> None:
         """ Constructor for HS110 data
-        hardware_version: defaults to 'h2' can also be 'h1' """
-
-        allowed_hardware = ['h1', 'h2']
-        if hardware_version in allowed_hardware:
-            self.__hardware = hardware_version
-        else:
-            raise ValueError("hardware_version must be " + ' or '.join(allowed_hardware))
+        hardware_version: defaults to 'h2' can also be 'h1' 
+        port: hss110 target port, for h1 and h2 is 9999,
+        """
+        self.__hardware = hardware_version
 
         self.__keyname = {
             "h1": { # Hardware version 1.x
@@ -64,36 +65,41 @@ class HS110data:
         self.__ip = ip
         self.__port = port
 
-    def __encrypt(self, string):
+    @require("The encrypt parameter must be str type", lambda args: isinstance(args.string, str))
+    @require("String must not be empty", lambda args: len(args.string) > 0)
+    @ensure("Result must be bytes", lambda args, result: isinstance(result, bytes))
+    def __encrypt(self, string: str) -> bytes:
         """ Encrypts string to send to HS110 """
-        if type(string) not in [str]:
-            raise TypeError("The encrypt parameter must be a string")
         key = self.__hs110_key
         result =  b"\0\0\0" + bytes([len(string)])
-        for i in bytes(string.encode('latin-1')):
+        for i in bytes(string.encode('latin-1', 'replace')):
             a = key ^ i
             key = a
             result += bytes([a])
         return result
 
-    def __decrypt(self, string):
+    @require("The decrypt parameter must be bytes type", lambda args: isinstance(args.data, bytes))
+    @require("Parameter must have more than 3 bytes starting with 000", lambda args: len(args.data) > 3 and args.data[0:3] == b"\0\0\0")
+    @ensure("Result must be str", lambda args, result: isinstance(result, str))
+    def __decrypt(self, data: bytes) -> str:
         """ Decrypts bytestring received by HS110 """
-        if type(string) != bytes:
-            raise TypeError("The decrypt parameter must be bytes")
-        string= string[4:]
+        data= data[4:]
         key = self.__hs110_key
         result = b""
-        for i in bytes(string):
+        for i in bytes(data):
             a = key ^ i
             key = i
             result += bytes([a])
-        return result.decode('latin-1')
+        return result.decode('latin-1', 'replace')
 
-    def __str__(self):
+    @ensure("Result must be str", lambda args, result: isinstance(result, str))
+    @ensure("Result str must not be empty", lambda args, result: len(result) > 0)
+    def __str__(self) -> str:
         """ Prints content of received HS110 data """
         return ', '.join(['{key}={value}'.format(key=key, value=self.__received_data['emeter']['get_realtime'].get(key)) for key in self.__received_data['emeter']['get_realtime']])
 
-    def __empty_data(self):
+    @ensure("Result must be dict", lambda args, result: isinstance(result, dict))
+    def __empty_data(self) -> dict:
         """ Clear received data to 0 values """
         return {
             "emeter": {
@@ -107,10 +113,10 @@ class HS110data:
             }
         }
 
-    def receive(self, data):
+    @require("Parameter data must be bytes type", lambda args: isinstance(args.data, bytes))
+    @require("Parameter must have more than 3 bytes starting with 000", lambda args: len(args.data) > 3 and args.data[0:3] == b"\0\0\0")
+    def receive(self, data: bytes) -> None:
         """ Receive encrypted data, decrypts and stores into self.reived_data """
-        if type(data) is not bytes:
-            raise ValueError("receive_data parameter must be of type bytes")
         try:
             self.__received_data = json.loads(self.__decrypt(data))
         except:
@@ -121,32 +127,34 @@ class HS110data:
         else:
             self.__hardware = 'h1'
 
-    def get_cmd(self):
+    @ensure("Result must be a bytes", lambda args, result: isinstance(result, bytes))
+    def get_cmd(self) -> bytes:
         """ Get encrypted command to get realtime info from HS110 """
         cmd = '{"emeter":{"get_realtime":{}}}'
         return self.__encrypt(cmd)
 
-    def get_data(self, item):
+    @require("Parameter data must be str type", lambda args: isinstance(args.item, str))
+    @ensure("Result must be a float or int", lambda args, result: isinstance(result, float) or isinstance(result, int)  )
+    def get_data(self, item: str) -> Union[float, int]:
         """ Get item (power, current, voltage or total) from HS110 array of values """
-        if type(item) is not str:
-            raise TypeError('get_data parameter must be str type')
         try:
-            return self.__received_data["emeter"]["get_realtime"][self.__keyname[self.__hardware][item]]
+            return float(self.__received_data["emeter"]["get_realtime"][self.__keyname[self.__hardware][item]])
         except KeyError:
             raise KeyError('get_data parameter must be one of: [' + ', '.join(self.__received_data["emeter"]["get_realtime"].keys()) + ']')
 
-    def get_connection_info(self):
+    @ensure("Result must be a string", lambda args, result: isinstance(result, str)  )
+    def get_connection_info(self) -> str:
         return 'HS110 connection: %s:%s' % (self.__ip, str(self.__port))
 
-    def reset_data(self):
+    def reset_data(self) -> None:
         """ Reset self.__received_data values to 0 """
         self.__received_data = self.__empty_data()
 
-    def connect(self):
+    def connect(self) -> None:
         """ Connect to hss110 with get command to receive metrics """
         self.send(self.get_cmd())
 
-    def send(self, command):
+    def send(self, command: bytes) -> None:
         """ Send command to hs110 and receive data """
         sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock_tcp.settimeout(2)
@@ -167,19 +175,11 @@ class HS110data:
             self.reset_data()
             print("[warning] Could not decrypt data from hs110. Reseting values.")
 
-# Main entry point
-if __name__ == '__main__':
-    # Parse commandline arguments
-    parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Prometheus exporter v" + str(version))
-    parser.add_argument("-t", "--target", metavar="<ip>", required=True, help="Target IP Address", type=validIP)
-    parser.add_argument("-f", "--frequency", metavar="<seconds>", required=False, help="Interval in seconds between checking measures", default=1, type=int)
-    parser.add_argument("-p", "--port", metavar="<port>", required=False, help="Port for listenin", default=8110, type=int)
-    args = parser.parse_args()
+@require("Parameter data must be argparse.Namespace type", lambda args: isinstance(args.args, argparse.Namespace))
+def main(args: argparse.Namespace) -> None:
 
     # Init object
     hs110 = HS110data(hardware_version='h2', ip=args.target)
-
-    # Send command and receive reply
 
     # Create a metric to track time spent and requests made.
     # Gaugage: it goes up and down, snapshot of state
@@ -206,3 +206,14 @@ if __name__ == '__main__':
         hs110.connect()
         print('[info] %s' % hs110)
         time.sleep(args.frequency)
+
+# Main entry point
+if __name__ == '__main__':
+    # Parse commandline arguments
+    parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Prometheus exporter v" + str(version))
+    parser.add_argument("-t", "--target", metavar="<ip>", required=True, help="Target IP Address", type=validIP)
+    parser.add_argument("-f", "--frequency", metavar="<seconds>", required=False, help="Interval in seconds between checking measures", default=1, type=int)
+    parser.add_argument("-p", "--port", metavar="<port>", required=False, help="Port for listenin", default=8110, type=int)
+    args = parser.parse_args()
+
+    main(args)
--- test_hs110exporter.py
+++ test_hs110exporter.py
@@ -2,23 +2,51 @@
 
 import unittest
 import socket
-try:
-    from mock import patch, call  # python 3
-except ImportError:
-    from unittest.mock import patch, call  # python 3
+import time
+import argparse
 
-from hs110exporter import validIP, HS110data, socket
+from unittest.mock import patch, call, MagicMock  # Python 3
+
+from dpcontracts import require, ensure, PreconditionError
+from hypothesis import given, assume, example
+from hypothesis.strategies import integers, one_of, floats, text, lists, none, permutations, complex_numbers
+from hs110exporter import validIP, HS110data, socket, main
 
 class TestValidIP(unittest.TestCase):
   def test_ipstring(self):
     self.assertEqual(validIP('192.168.0.1'), '192.168.0.1')
     self.assertEqual(validIP(' 192.168.0.1 '), '192.168.0.1')
 
+  @given(none())
+  @example(b'\x00')
+  @example(100)
+  @example(100.1)
+  @example(3j)
+  @example({"string into dict"})
+  @example(set("string into set"))
+  def test_ipstring_types(self, fake_ip_type):
+    self.assertRaises(PreconditionError, validIP, fake_ip_type)
+
+#  @given(text())
+#  @example("192.168.0.1.a")
+#  @example("192.168.0.1.256")
   def test_ipvalues(self):
-    self.assertRaises(TypeError, validIP, 192)
-    self.assertRaises(TypeError, validIP, 192.168)
-    self.assertRaises(TypeError, validIP, 3j)
-    self.assertRaises(ValueError, validIP, '192.168.0.1.a')
+    self.assertRaises(ValueError, validIP, "192.168.0.a")
+
+  @given(integers(min_value=1, max_value=255),
+    integers(min_value=1, max_value=255),
+    integers(min_value=1, max_value=255),
+    integers(min_value=1, max_value=255)
+  )
+  def test_ipvalues_hypo(self, a, b, c, d):
+    ip_string = "%s.%s.%s.%s" % (str(a), str(b), str(c), str(d))
+    ip_string == validIP(ip_string) 
+
+  @given(integers(min_value=-1000, max_value=1000))
+  def test_ipvalues_hypo_raises(self, a):
+    if (a < 0 or a > 255):
+      ip_string = "%s.%s.%s.%s" % (str(a), str(0), str(1), str(2))
+      self.assertRaises(ValueError, validIP, ip_string)
 
 class TestHS110data(unittest.TestCase):
   def test_encryptstring(self):
@@ -29,39 +57,58 @@ class TestHS110data(unittest.TestCase):
     self.assertEqual(hs110._HS110data__encrypt(text_decrypted), text_encrypted)
     self.assertEqual(hs110._HS110data__decrypt(text_encrypted), text_decrypted)
 
-
-  def test_encryptvalues(self):
+  @given( one_of(
+    floats(),
+    none(),
+    text()
+  ))
+  @example(b'\x00\x00\x00\x10')
+  @example(100)
+  @example(3j)
+  @example({"10.1.1.1"})
+  @example(set("10.1.1.1"))
+  def test_encryptvalues(self, sample_type):
     hs110 = HS110data()
+    if (isinstance(sample_type, str) and len(sample_type) > 0):
+      self.assertIsInstance(hs110._HS110data__encrypt(sample_type), bytes)
+    else:
+      self.assertRaises(PreconditionError, hs110._HS110data__encrypt, sample_type)
 
-    self.assertRaises(TypeError, hs110._HS110data__encrypt, 100)
-    self.assertRaises(TypeError, hs110._HS110data__encrypt, 100.1)
-    self.assertRaises(TypeError, hs110._HS110data__encrypt, 3j)
-    self.assertRaises(TypeError, hs110._HS110data__encrypt, b'\x00\x00\x00\x10')
-    self.assertIsInstance(hs110._HS110data__encrypt('Hello world'), bytes)
-
-  def test_decryptvalues(self):
+  @given(none())
+  @example(b'\x00\x00\x00\x10')
+  @example(100)
+  @example(100.1)
+  @example(3j)
+  @example({"10.1.1.1"})
+  @example(set("10.1.1.1"))
+  @example("Hello world")
+  def test_decryptvalues(self, data):
     hs110 = HS110data()
+    if (isinstance(data,bytes) and data[0:3] == b"\0\0\0"):
+      self.assertIsInstance(hs110._HS110data__decrypt(data), str)
+    else:
+      self.assertRaises(PreconditionError, hs110._HS110data__decrypt, data)
 
-    self.assertRaises(TypeError, hs110._HS110data__decrypt, 100)
-    self.assertRaises(TypeError, hs110._HS110data__decrypt, 100.1)
-    self.assertRaises(TypeError, hs110._HS110data__decrypt, 3j)
-    self.assertRaises(TypeError, hs110._HS110data__decrypt, "Hello world")
-    self.assertIsInstance(hs110._HS110data__decrypt(b'\x00\x00\x00\x10'), str)
-
-  def test_received_data(self):
-
+  @given(one_of(
+    none(),
+    text())
+  )
+  @example(100)
+  @example(100.1)
+  @example(3j)
+  @example('power')
+  @example('current')
+  @example('voltage')
+  @example('total')
+  def test_received_data(self, data_item):
     for h in ['h1', 'h2']:
       hs110 = HS110data(h)
-      self.assertRaises(TypeError, hs110.get_data, 100)
-      self.assertRaises(TypeError, hs110.get_data, 100.1)
-      self.assertRaises(TypeError, hs110.get_data, 3j)
-      self.assertRaises(KeyError, hs110.get_data, 'nonexist')
-
-      self.assertEqual(hs110.get_data('power'), 0)
-      self.assertEqual(hs110.get_data('current'), 0)
-      self.assertEqual(hs110.get_data('voltage'), 0)
-      self.assertEqual(hs110.get_data('total'), 0)
-
+      if data_item in ['power', 'current', 'voltage', 'total']:
+        self.assertEqual(hs110.get_data(data_item), 0)
+      elif isinstance(data_item,str):
+        self.assertRaises(KeyError, hs110.get_data, data_item)
+      else:
+        self.assertRaises(PreconditionError, hs110.get_data, data_item)
 
   def test_receive(self):
     # current=0.342122, voltage=239.527888, power=66.941523, total=10.155, err_code=0
@@ -72,74 +119,92 @@ class TestHS110data(unittest.TestCase):
     sample_data_h2 = b'\x00\x00\x00\x82\xd0\xf2\x97\xfa\x9f\xeb\x8e\xfc\xde\xe4\x9f\xbd\xda\xbf\xcb\x94\xe6\x83\xe2\x8e\xfa\x93\xfe\x9b\xb9\x83\xf8\xda\xb9\xcc\xbe\xcc\xa9\xc7\xb3\xec\x81\xe0\xc2\xf8\xc8\xe6\xd5\xe1\xd3\xe2\xd0\xe2\xce\xec\x9a\xf5\x99\xed\x8c\xeb\x8e\xd1\xbc\xca\xe8\xd2\xe0\xd3\xea\xc4\xf1\xc3\xf4\xcc\xf4\xcc\xe0\xc2\xb2\xdd\xaa\xcf\xbd\xe2\x8f\xf8\xda\xe0\xd6\xe0\xce\xf7\xc3\xf2\xc7\xf5\xc6\xea\xc8\xbc\xd3\xa7\xc6\xaa\xf5\x82\xea\xc8\xf2\xc3\xf3\xdd\xec\xd9\xec\xdc\xec\xdc\xf0\xd2\xb7\xc5\xb7\xe8\x8b\xe4\x80\xe5\xc7\xfd\xcd\xb0\xcd\xb0'
     hs110 = HS110data()
 
-    self.assertRaises(ValueError, hs110.receive, 'this is a string')
-    self.assertRaises(ValueError, hs110.receive, 123)
-    self.assertRaises(ValueError, hs110.receive, 1.1)
-    self.assertRaises(ValueError, hs110.receive, 3j)
+    self.assertRaises(PreconditionError, hs110.receive, 'this is a string')
+    self.assertRaises(PreconditionError, hs110.receive, 123)
+    self.assertRaises(PreconditionError, hs110.receive, 1.1)
+    self.assertRaises(PreconditionError, hs110.receive, 3j)
     self.assertRaises(ValueError, hs110.receive, sample_data_fail)
     hs110.receive(sample_data_ok)
     hs110.receive(sample_data_h2)
 
+  @given(text())
+  @example('h1')
+  @example('h2')
+  def test_constructor(self, hardware):
 
-  def test_constructor(self):
-    h1_empty_print = 'current=0, voltage=0, power=0, total=0, err_code=0'
-    h1_empty = {
-      "emeter": {
-        "get_realtime": {
-          "current": 0,
-          "voltage": 0,
-          "power": 0,
-          "total": 0,
-          "err_code": 0
+    if (hardware in ['h1', 'h2']):
+      empty_print = {
+        'h1': 'current=0, voltage=0, power=0, total=0, err_code=0',
+        'h2': 'current_ma=0, voltage_mv=0, power_mw=0, total_wh=0, err_code=0'
+      }
+      empty_value = {
+        'h1': {
+          "emeter": {
+            "get_realtime": {
+              "current": 0,
+              "voltage": 0,
+              "power": 0,
+              "total": 0,
+              "err_code": 0
+            }
+          }
+        },
+        'h2': {
+          "emeter": {
+            "get_realtime": {
+              "current_ma": 0,
+              "voltage_mv": 0,
+              "power_mw": 0,
+              "total_wh": 0,
+              "err_code": 0
+            }
+          }
         }
       }
-    }
 
-    h2_empty_print = 'current_ma=0, voltage_mv=0, power_mw=0, total_wh=0, err_code=0'
-    h2_empty = {
-      "emeter": {
-        "get_realtime": {
-          "current_ma": 0,
-          "voltage_mv": 0,
-          "power_mw": 0,
-          "total_wh": 0,
-          "err_code": 0
-        }
-      }
-    }
+      hs110 = HS110data(hardware)
+      self.assertEqual(hs110._HS110data__hardware, hardware)
+      self.assertEqual(hs110._HS110data__received_data, empty_value[hardware])
+      self.assertEqual(str(hs110), empty_print[hardware])
 
-    self.assertRaises(ValueError, HS110data, hardware_version='h999')
+      self.assertEqual(hs110._HS110data__hardware, hardware)
+      hs110._HS110data__received_data['emeter']['get_realtime'][hs110._HS110data__keyname[hardware]['current']] = 1
+      hs110._HS110data__received_data['emeter']['get_realtime'][hs110._HS110data__keyname[hardware]['voltage']] = 220
+      hs110._HS110data__received_data['emeter']['get_realtime'][hs110._HS110data__keyname[hardware]['power']] = 220
+      hs110._HS110data__received_data['emeter']['get_realtime'][hs110._HS110data__keyname[hardware]['total']] = 1.3
+      hs110._HS110data__received_data['emeter']['get_realtime']['err_code'] = 1
 
-    hs110 = HS110data('h1')
-    self.assertEqual(hs110._HS110data__hardware, 'h1')
-    self.assertEqual(hs110._HS110data__received_data, h1_empty)
-    self.assertEqual(str(hs110), h1_empty_print)
+      hs110.reset_data()
+      self.assertEqual(hs110._HS110data__received_data, empty_value[hardware])
 
-    hs110 = HS110data('h2')
-    self.assertEqual(hs110._HS110data__hardware, 'h2')
-    self.assertEqual(hs110._HS110data__received_data, h2_empty)
-    self.assertEqual(str(hs110), h2_empty_print)
+    else:
 
-    hs110 = HS110data()
-    self.assertEqual(hs110._HS110data__hardware, 'h2')
+      self.assertRaises(PreconditionError, HS110data, hardware)
 
-    hs110._HS110data__received_data['emeter']['get_realtime']['current_ma'] = 1
-    hs110._HS110data__received_data['emeter']['get_realtime']['voltate_mv'] = 220
-    hs110._HS110data__received_data['emeter']['get_realtime']['power_mw'] = 220
-    hs110._HS110data__received_data['emeter']['get_realtime']['total_wh'] = 1.3
-    hs110._HS110data__received_data['emeter']['get_realtime']['err_code'] = 1
-    hs110.reset_data()
-    self.assertEqual(hs110._HS110data__received_data, h2_empty)
-
+  @given(integers(min_value=-100, max_value=70000))
+  @example(9999)
+  def test_port(self, port_number):
+    if (port_number <0 or port_number >65535):
+      self.assertRaises(PreconditionError, HS110data, port=port_number)
+    else:
+      hs110 = HS110data(port=port_number)
+      self.assertEqual(hs110._HS110data__port, port_number)
+  
   def test_get_cmd(self):
     cmd_encrypted = b'\x00\x00\x00\x1e\xd0\xf2\x97\xfa\x9f\xeb\x8e\xfc\xde\xe4\x9f\xbd\xda\xbf\xcb\x94\xe6\x83\xe2\x8e\xfa\x93\xfe\x9b\xb9\x83\xf8\x85\xf8\x85'
     hs110 = HS110data()
 
     self.assertEqual(hs110.get_cmd(), cmd_encrypted)
 
-  def test_get_connection_info(self):
-    test_ip   = '192.168.1.99'
-    test_port = 9998
+  @given(
+    integers(min_value=1, max_value=255),    # IPv4 1st group
+    integers(min_value=1, max_value=255),    # IPv4 2nd group
+    integers(min_value=1, max_value=255),    # IPv4 3rd group
+    integers(min_value=1, max_value=255),    # IPv4 4th group
+    integers(min_value=0, max_value=65535))  # HS110 Port
+  @example(192, 168, 1, 99, 9998)
+  def test_get_connection_info(self, ip1, ip2, ip3, ip4, test_port):
+    test_ip   = '%s.%s.%s.%s' % (ip1, ip2, ip3, ip4)
     info  =  "HS110 connection: %s:%s" % (test_ip, test_port)
     hs110 = HS110data(ip = test_ip, port = test_port)
 
@@ -158,8 +223,7 @@ class TestHS110data(unittest.TestCase):
   @patch.object(socket.socket,'recv')
   @patch.object(socket.socket,'close')
   @patch.object(socket.socket,'__init__')
-  @patch('builtins.print')
-  def test_socket(self, mock_print, mock_init, mock_close, mock_recv, mock_send, mock_connect, mock_settimeout):
+  def test_socket(self, mock_init, mock_close, mock_recv, mock_send, mock_connect, mock_settimeout):
     assert socket.socket.settimeout is mock_settimeout
     assert socket.socket.connect is mock_connect
     assert socket.socket.send is mock_send
@@ -189,18 +253,48 @@ class TestHS110data(unittest.TestCase):
 
     # Test socket exception from send
     mock_recv.side_effect = ValueError()
-    hs110.send('mycommand')
-    self.assertEqual(hs110._HS110data__received_data, hs110._HS110data__empty_data())
-    assert mock_print.mock_calls == [
-      call('[warning] Could not decrypt data from hs110. Reseting values.')
-    ]
+    with patch('builtins.print') as mock_print:
+      hs110.send('mycommand')
+
+      self.assertEqual(hs110._HS110data__received_data, hs110._HS110data__empty_data())
+      assert mock_print.mock_calls == [
+        call('[warning] Could not decrypt data from hs110. Reseting values.')
+      ]
 
     mock_connect.side_effect = socket.error()
-    hs110.send('mycommand')
-    assert mock_print.mock_calls == [
-      call('[warning] Could not decrypt data from hs110. Reseting values.'),
-      call('[error] Could not connect to the host 192.168.1.100:9991 Keeping last values')
-    ]
+    with patch('builtins.print') as mock_print:
+      hs110.send('mycommand')
+      assert mock_print.mock_calls == [
+        call('[error] Could not connect to the host 192.168.1.100:9991 Keeping last values')
+      ]
+
+  @patch('time.sleep')
+  @patch.object(HS110data, 'connect')
+  @patch('hs110exporter.start_http_server')
+  def test_main(self, mock_http_server, mock_connect, mock_sleep):
+    assert time.sleep is mock_sleep
+    assert HS110data.connect is mock_connect
+    mock_sleep.side_effect = Exception("Ignore it... just connect mock")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", metavar="<ip>", required=False, help="Target IP Address", default="192.168.1.1", type=str)
+    parser.add_argument("-f", "--frequency", metavar="<seconds>", required=False, help="Interval in seconds between checking measures", default=1, type=int)
+    parser.add_argument("-p", "--port", metavar="<port>", required=False, help="Port for listenin", default=8110, type=int)
+    args = parser.parse_args()
+
+    with patch('builtins.print') as mock_print:
+      assert mock_print is mock_print
+      self.assertRaises(Exception, main, args)
+
+      assert mock_print.mock_calls == [
+        call("[info] HS110 connection: 192.168.1.1:9999"),
+        call("[info] Exporter listenting on TCP: 8110"),
+        call("[info] current_ma=0, voltage_mv=0, power_mw=0, total_wh=0, err_code=0")
+      ]
+
+    mock_connect.assert_called_once()
+    assert mock_http_server.mock_calls == [ call(args.port) ]
+    assert mock_sleep.mock_calls == [ call(args.frequency) ]
 
 if __name__ == '__main__':
     unittest.main()
